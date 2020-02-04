459122abb82cb46b2c6c7d9663fdd3ad6039353e
SeungHun Han <shhan730@gmail.com>
2020-02-04 20:35:58 +0900

    Set index

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
+var app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}));
 
-require('./config/passport')(passport);
+require('./api/auth/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session()); //로그인 세션 유지
 
